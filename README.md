512406e3ce1a9add16c215513325411abca1a50f
Raphael Felber <coeps@coeps.com>
2019-06-20 22:32:01 +0200

    Using rc and debug like dns-proxy, added readme etc.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # https-reverse-proxy
 simple configurable https reverse proxy
+
+## Config
+-> https://www.npmjs.com/package/rc
+
+https-reverse-proxy --config config.json
+
+## Default Config
+```json
+{
+    "logging": "httpsreverseproxy:info",
+    "https": {
+        "key": "./certificates/private.key",
+        "cert": "./certificates/public-certificate.pem"
+    },
+    "servers": [
+        {
+            "port": 8443,
+            "rules": [
+                {
+                    "path": "/*",
+                    "target": "https://www.google.ch",
+                    "removePathFromRequestUrl": false
+                }
+            ]
+        }
+    ],
+    "reloadConfig": false
+}
+```
+
+##Limitations
+If removePathFromRequestUrl is true, the path is removed from the request.url and then handed over to the proxy.
+The path ending / or /* is removed and the resulting string is removed from the request.url.
+Therefore no complex paths (e.g. wildcards in between, regexp) are possible with this option enabled.
--- index.js
+++ index.js
@@ -1,86 +1,116 @@
-const program = require('commander');
+const rc = require('rc');
 const fs = require('fs');
 const express = require('express');
 const https = require('https');
 const httpProxy = require('http-proxy');
 
-let configFilePath = '';
-program.version('1.0.0')
-    .arguments('<configFilePath>')
-    .action((cfg) => {
-        configFilePath = cfg;
-    })
-    .parse(process.argv);
+const defaults = {
+    logging: "httpsreverseproxy:info",
+    https: {
+        key: "./certificates/private.key",
+        cert: "./certificates/public-certificate.pem"
+    },
+    servers: [
+        {
+            port: 8443,
+            rules: [
+                {
+                    path: "/*",
+                    target: "https://www.google.ch",
+                    removePathFromRequestUrl: false
+                }
+            ]
+        }
+    ],
+    reloadConfig: false
+};
 
-if (!configFilePath) {
-    console.error('\nConfig file path parameter missing in args >%s<\n', program.args.join(' '));
-    program.outputHelp();
-    process.exit(1);
-}
+let openServers = [];
+let config = rc('httpsreverseproxy', defaults);
 
-const file = fs.readFileSync(configFilePath, 'utf8');
-const config = JSON.parse(file);
+process.env.DEBUG_FD = process.env.DEBUG_FD || 1;
+process.env.DEBUG = process.env.DEBUG || config.logging;
+const d = process.env.DEBUG.split(',');
+d.push('httpsreverseproxy:error');
+process.env.DEBUG = d.join(',');
 
-if (!config.https ||
-    !config.https.key ||
-    !config.https.cert ||
-    !config.servers ||
-    !Array.isArray(config.servers) ||
-    config.servers.length < 1 ||
-    !config.servers[0].rules ||
-    !Array.isArray(config.servers[0].rules) ||
-    config.servers[0].rules.length < 1 ||
-    !config.servers[0].rules[0].path ||
-    !config.servers[0].rules[0].target) {
-    console.error('\nConfig format must be like: \n' +
-        '{\n' +
-        '  "https": {\n' +
-        '    "key": "./certificates/private.key",\n' +
-        '    "cert": "./certificates/public-certificate.pem"\n' +
-        '  },\n' +
-        '  "servers": [\n' +
-        '    {\n' +
-        '      "port": 7443,\n' +
-        '      "rules": [\n' +
-        '        {\n' +
-        '          "path": "/*",\n' +
-        '          "target": "https://www.google.com"\n' +
-        '        }\n' +
-        '      ]\n' +
-        '    }\n' +
-        '  ]\n' +
-        '}\n\n', configFilePath);
-    program.outputHelp();
-    process.exit(1);
-}
+const debug = require('debug');
+const logdebug = debug('httpsreverseproxy:debug');
+const loginfo = debug('httpsreverseproxy:info');
+const logerror = debug('httpsreverseproxy:error');
 
-config.servers.forEach(server => {
-    const proxy = httpProxy.createProxyServer({
-        key: fs.readFileSync(config.https.key, 'utf8'),
-        cert: fs.readFileSync(config.https.cert, 'utf8'),
-        secure: false,
-        changeOrigin: true,
-        autoRewrite: true,
-        followRedirects: true
-    });
+const run = () => {
+    logdebug('options: %j', config);
 
-    const app = express();
-
-    const httpsServer = https.createServer({
-        key: fs.readFileSync(config.https.key, 'utf8'),
-        cert: fs.readFileSync(config.https.cert, 'utf8'),
-        rejectUnauthorized: false,
-        requestCert: false
-    }, app);
-
-    server.rules.forEach(rule => {
-        const path = rule.path.replace(/\/$|\/\*$/, '');
-        app.all(rule.path, (req, res) => {
-            const url = req.url.replace(path, '');
-            req.url = url === '' ? '/' : url;
-            proxy.web(req, res, {target: rule.target});
+    config.servers.forEach(server => {
+        const proxy = httpProxy.createProxyServer({
+            key: fs.readFileSync(config.https.key, 'utf8'),
+            cert: fs.readFileSync(config.https.cert, 'utf8'),
+            secure: false,
+            changeOrigin: true,
+            autoRewrite: true,
+            followRedirects: true
         });
-    });
+        openServers.push(proxy);
 
-    httpsServer.listen(server.port);
-});
+        const app = express();
+
+        const httpsServer = https.createServer({
+            key: fs.readFileSync(config.https.key, 'utf8'),
+            cert: fs.readFileSync(config.https.cert, 'utf8'),
+            rejectUnauthorized: false,
+            requestCert: false
+        }, app);
+
+        server.rules.forEach(rule => {
+            const path = rule.path.replace(/\/$|\/\*$/, '');
+            app.all(rule.path, (req, res) => {
+                if (rule.removePathFromRequestUrl) {
+                    const url = req.url.replace(path, '');
+                    req.url = url === '' ? '/' : url;
+                }
+                proxy.web(req, res, {target: rule.target});
+            });
+        });
+
+        httpsServer.listen(server.port);
+        openServers.push(httpsServer);
+    });
+};
+
+const close = () => {
+    openServers.forEach(server => server.close());
+    openServers = [];
+};
+
+if (config.reloadConfig === true) {
+    const configFile = config.config;
+    fs.watchFile(configFile, () => {
+        loginfo('config file changed, reloading config options');
+        try {
+            close();
+        } catch (e) {
+            logerror('error closing servers');
+            logerror(e)
+        }
+        try {
+            config = rc('httpsreverseproxy', defaults);
+        } catch (e) {
+            logerror('error reloading configuration');
+            logerror(e)
+        }
+        try {
+            run();
+        } catch (e) {
+            logerror('error running servers');
+            logerror(e)
+        }
+    })
+}
+
+try {
+    run();
+} catch (e) {
+    logerror('error running servers');
+    logerror(e)
+}
